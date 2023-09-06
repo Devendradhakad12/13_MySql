efa33849e5d9983fafa918612c24e433ec941864
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-09-07 03:34:25 +0530

    new features

--- Express_With_Sql/index.js
+++ Express_With_Sql/index.js
@@ -1,11 +1,14 @@
-const express = require("express")
-const mysql = require("mysql2")
+const express = require("express");
+const mysql = require("mysql2");
+const methodOverride = require("method-override");
 const app = express();
 const PORT = 8080;
-const path = require("path")
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname,"/views"))
-app.use(express.json())
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // connect to MYSQL Database
 const connection = mysql.createConnection({
@@ -15,39 +18,75 @@ const connection = mysql.createConnection({
   password: "@dev0101",
 });
 
-
-//  route get all users count from database
+//  route get users count from database
 
 app.get("/", (req, res) => {
   let q = `SELECT COUNT(*) FROM user`;
   try {
-    connection.query(q, (err, result) => {  
+    connection.query(q, (err, result) => {
       if (err) throw err;
-      let count = result[0]["COUNT(*)"]
-      
-     res.render("home.ejs", { count })
+      let count = result[0]["COUNT(*)"];
+
+      res.render("home.ejs", { count });
     });
   } catch (error) {
-    res.send("some error in Database")
-  } 
+    res.send("some error in Database");
+  }
 });
 
-//  route get all users from database
+//  route Get all users from database
 app.get("/users", (req, res) => {
   let q = `SELECT * FROM user`;
   try {
-    connection.query(q, (err, result) => {  
+    connection.query(q, (err, result) => {
       if (err) throw err;
-      res.send(result);
+      res.render("users.ejs", { result });
     });
   } catch (error) {
-    res.send("some error in Database")
-  } 
+    res.send("some error in Database");
+  }
 });
 
+//  route Edit username form
+app.get("/user/:id/edit", (req, res) => {
+  let id = req.params.id;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      res.render("edit.ejs", { user });
+    });
+  } catch (error) {
+    res.send("some error in Database");
+  }
+});
+
+//  route Update username from database
+app.patch("/user/:id", (req, res) => {
+  let id = req.params.id;
+  let { password: formPass, username: newusername } = req.body;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      if (formPass !== user.password) {
+        res.send("wrong password");
+      } else {
+        let q2 = `UPDATE user SET username='${newusername}' WHERE id="${id}"`;
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          res.redirect("/users"); 
+        });
+      }
+    });
+  } catch (error) {
+    res.send("some error in Database");
+  }
+});
 
 // connect to server
 app.listen(PORT, () => {
   console.log(`app listining on http://localhost:${PORT}`);
 });
- 
--- Express_With_Sql/schema.sql
+++ Express_With_Sql/schema.sql
@@ -4,3 +4,7 @@ USE delta_app;
 SELECT * FROM user;
 
 SELECT COUNT(*) FROM user;
+
+SELECT * FROM user WHERE id="fcd05f1e-8352-4215-911b-a759991992d9"
+
+UPDATE user SET username="newname" WHERE id="fcd05f1e-8352-4215-911b-a759991992d9";
